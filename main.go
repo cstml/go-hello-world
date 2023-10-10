a1d5859570a54873b776996616cbb7caac3c4109
Vlad P. Luchian <cstmlcodes@gmail.com>
2023-10-10 20:00:40 +0100

    update.

--- main.go
+++ main.go
@@ -1,17 +1,8 @@
 package main
 
-import "fmt"
 import greet "github.com/cstml/go-hello-world/greeting"
-
 
 // Say hello.
 func main () {
-  fmt.Println(greet.Greet, "!")
-  c := make (chan int)
-  go func () {
-    Greet := "Hey World!"
-    fmt.Println(Greet)
-    c <- 0
-  }()
-  <-c
+  SayHello(greet.Greet)
 }
---
+++ sayHello.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func SayHello(s string) {
+	  fmt.Println(s, "!")
+}
